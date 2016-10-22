bdaac3229a2dc3bfc8ae7df7946c7af2e3ea6126
Tim Kuzhagaliyev <tim.kuzh@gmail.com>
2016-10-22 19:55:10 +0100

    Add routing

--- index.js
+++ index.js
@@ -11,8 +11,16 @@ const PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 
+/**
+ * Show a nice welcome message to all request without the username specified, and point them at the GitHub page.
+ * @since 0.0.1
+ */
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('index.html');
+});
+
+app.get('/:username(:extension)?', function (req, res) {
+    res.send(req.params);
 });
 
 app.listen(PORT, function () {
